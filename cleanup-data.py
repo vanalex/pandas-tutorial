6eca2c9e50c3e659e6a36c292cee81273be1c76f
vanalex <apjorquera@gmail.com>
2018-03-15 19:00:57 +0100

    add cleanup data

--- cleanup-data.py
+++ cleanup-data.py
@@ -16,3 +16,25 @@ print(requests['Incident Zip'].unique())
 rows_with_dashes = requests['Incident Zip'].str.contains('-').fillna(False)
 print('##################################################################')
 print(requests[rows_with_dashes])
+
+# requests['Incident Zip'][rows_with_dashes] = np.nan
+
+long_zip_codes = requests['Incident Zip'].str.len() > 5
+requests['Incident Zip'][long_zip_codes].unique()
+print('')
+print(requests['Incident Zip'][long_zip_codes].unique())
+
+print('')
+requests['Incident Zip'] = requests['Incident Zip'].str.slice(0, 5)
+print(requests['Incident Zip'])
+
+print('')
+print(requests[requests['Incident Zip'] == '00000'])
+
+zero_zips = requests['Incident Zip'] == '00000'
+requests['Incident Zip'][zero_zips] = np.nan
+
+unique_zips = requests['Incident Zip'].unique()
+print('')
+print(unique_zips.sort())
+print(unique_zips)
